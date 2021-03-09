908e415185885c847b1dca5265da76edc223564c
Marco <markforester93@gmail.com>
2021-03-10 00:20:03 +1100

    Third Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// function happyFace(elmnt){
+//     elmnt.style = 'background-color:red;';
+//     console.log (elmnt.id);
+//     window.getComputedStyle(
+//         document.querySelector('.element'), ':before'
+//     ).getPropertyValue('color')
+//     window.getComputedStyle(document.querySelector(elmnt.id),':after').setProperty('display','initial');
+//     window.getComputedStyle(document.querySelector(elmnt.id),':after').setProperty('animation-name','fly');
+// }
